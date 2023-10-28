49af9518b04bd3610251e3842c150e928a443ba7
Mislav Matijević <mislav.mim@gmail.com>
2023-10-28 13:37:14 +0200

    Move RegistrationPage to a "navigation.components" package. By introducing a "navigation" package, it becomes possible to clearly state which composable functions are just UI components, and which are actually fully designed views/pages. The word "Page" does refer to web pages, but it is conceptually related to a view that pops up and displays something to a user. Alternative names could be "View", "Window" or "Form". "View" sounds like a View class from older Android development. "Window" is completely out of the picture due it coming from Windows OS. Another name would be "Form", but not all UI views are going to be forms to be filled out, some are rather just neat displays of data from the backend/DB. Naming packages is not easy, find what suits your project the best.

--- app/src/main/java/hr/foi/air/popapp/MainActivity.kt
+++ app/src/main/java/hr/foi/air/popapp/MainActivity.kt
@@ -3,20 +3,11 @@ package hr.foi.air.popapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.unit.dp
-import hr.foi.air.popapp.ui.components.PasswordTextField
-import hr.foi.air.popapp.ui.components.StyledButton
-import hr.foi.air.popapp.ui.components.StyledTextField
+import hr.foi.air.popapp.navigation.components.RegistrationPage
 import hr.foi.air.popapp.ui.theme.POPAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,64 +21,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun RegistrationPage() {
-    var firstName by remember {
-        mutableStateOf("")
-    }
-    var lastName by remember {
-        mutableStateOf("")
-    }
-    var username by remember {
-        mutableStateOf("")
-    }
-    var email by remember {
-        mutableStateOf("")
-    }
-    var password by remember {
-        mutableStateOf("")
-    }
-    var confirmPassword by remember {
-        mutableStateOf("")
-    }
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-            .verticalScroll(rememberScrollState()),
-    ) {
-        Text(
-            text = "Create an Account",
-            style = MaterialTheme.typography.h4,
-            modifier = Modifier.padding(vertical = 16.dp)
-        )
-
-        StyledTextField(label = "First name", value = firstName, onValueChange = { firstName = it })
-
-        StyledTextField(label = "Last name", value = lastName, onValueChange = { lastName = it })
-
-        StyledTextField(label = "Username", value = username, onValueChange = { username = it })
-
-        StyledTextField(label = "Email", value = email, onValueChange = { email = it })
-
-        PasswordTextField(label = "Password", value = password, onValueChange = { password = it })
-
-        PasswordTextField(
-            label = "Confirm password",
-            value = confirmPassword,
-            onValueChange = { confirmPassword = it },
-            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done)
-        )
-
-        Spacer(modifier = Modifier.height(50.dp))
-
-        StyledButton(
-            label = "Register",
-            onClick = {}
-        )
     }
 }
---
+++ app/src/main/java/hr/foi/air/popapp/navigation/components/RegistrationPage.kt
@@ -0,0 +1,75 @@
+package hr.foi.air.popapp.navigation.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import hr.foi.air.popapp.ui.components.PasswordTextField
+import hr.foi.air.popapp.ui.components.StyledButton
+import hr.foi.air.popapp.ui.components.StyledTextField
+
+
+@Composable
+fun RegistrationPage() {
+    var firstName by remember {
+        mutableStateOf("")
+    }
+    var lastName by remember {
+        mutableStateOf("")
+    }
+    var username by remember {
+        mutableStateOf("")
+    }
+    var email by remember {
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
+    var confirmPassword by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState()),
+    ) {
+        Text(
+            text = "Create an Account",
+            style = MaterialTheme.typography.h4,
+            modifier = Modifier.padding(vertical = 16.dp)
+        )
+
+        StyledTextField(label = "First name", value = firstName, onValueChange = { firstName = it })
+
+        StyledTextField(label = "Last name", value = lastName, onValueChange = { lastName = it })
+
+        StyledTextField(label = "Username", value = username, onValueChange = { username = it })
+
+        StyledTextField(label = "Email", value = email, onValueChange = { email = it })
+
+        PasswordTextField(label = "Password", value = password, onValueChange = { password = it })
+
+        PasswordTextField(
+            label = "Confirm password",
+            value = confirmPassword,
+            onValueChange = { confirmPassword = it },
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done)
+        )
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+        StyledButton(
+            label = "Register",
+            onClick = {}
+        )
+    }
+}
