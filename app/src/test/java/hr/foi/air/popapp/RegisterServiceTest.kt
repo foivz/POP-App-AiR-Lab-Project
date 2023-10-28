887d327d9e2499e3858894bd66a41a30763727aa
Mislav Matijević <mislav.mim@gmail.com>
2023-10-28 13:06:28 +0200

    Test the backend using its registration route. Backend is open sourced on the repo: github.com/foivz/pop-app-backend Backend is currently deployed on the URL ase.foi.hr/pop/api/v2 The test method tests whether the service is reachable and whether it works. The 'cvelasquez' username is already existing in the database, so a good way to test the inner workings of the backend is to try to register as an already existing user. If that fails, either a server is off, something changed in the API, or the standard mock user for whatever reason no longer exists.

---
+++ app/src/test/java/hr/foi/air/popapp/RegisterServiceTest.kt
@@ -0,0 +1,42 @@
+package hr.foi.air.popapp
+
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class RegisterServiceTest {
+    @Test
+    fun testRegistrationRoute() {
+        val existingUsername = "cvelasquez"
+
+        val url = "http://ase.foi.hr/pop/api/v2/auth/register"
+        val jsonForExistingUser = "{\n" +
+                "    \"first_name\": \"SomeName\",\n" +
+                "    \"last_name\": \"SomeSurname\",\n" +
+                "    \"username\": \"$existingUsername\",\n" +
+                "    \"email\": \"someemail@ase.foi.hr\",\n" +
+                "    \"password\": \"test123\",\n" +
+                "    \"role\": \"buyer\"\n" +
+                "}"
+        val client = OkHttpClient()
+
+        val requestBody = jsonForExistingUser.toRequestBody("application/json".toMediaType())
+        val request = Request.Builder()
+            .url(url)
+            .post(requestBody)
+            .build()
+
+        client.newCall(request).execute().body.also { res ->
+            val stringResponseBody = res?.string()!!
+            assertTrue(stringResponseBody.contains("Could not register user!"))
+        }
+    }
+}
