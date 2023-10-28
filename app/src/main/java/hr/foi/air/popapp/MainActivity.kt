bed4f43044cea0a77fe92cfd0151aac9227cc0b6
Mislav Matijević <mislav.mim@gmail.com>
2023-10-28 12:57:02 +0200

    Clean up the showcase code from MainActivity. All UI is going to be written from scratch, we don't need the "Greeting" composable from the project template.

--- app/src/main/java/hr/foi/air/popapp/MainActivity.kt
+++ app/src/main/java/hr/foi/air/popapp/MainActivity.kt
@@ -3,17 +3,10 @@ package hr.foi.air.popapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import hr.foi.air.popapp.ui.theme.POPAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,34 +16,9 @@ class MainActivity : ComponentActivity() {
             POPAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        var counter by remember { mutableStateOf(0) }
-        Text(text = "You've pressed a button $counter times")
-        Button(onClick = { counter++ }) {
-            Text(text = "Press me")
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    POPAppTheme {
-        Greeting("Android")
     }
 }
