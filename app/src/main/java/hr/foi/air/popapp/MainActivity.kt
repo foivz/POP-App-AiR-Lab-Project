8d7e38ef85d76b8af565ffd968b3bbac978faf61
Mislav Matijević <mislav.mim@gmail.com>
2023-10-28 13:18:03 +0200

    Design registration page. Currently, the registration page's composable function acts as a main view for the entire application. That ought to change with the introduction of navigation. No business logic for registering is yet implemented due to a lack of a more serious HTTP client dependency (can't use OkHttp3 as a main way to communicate with a web service, it's low level).

--- app/src/main/java/hr/foi/air/popapp/MainActivity.kt
+++ app/src/main/java/hr/foi/air/popapp/MainActivity.kt
@@ -3,10 +3,16 @@ package hr.foi.air.popapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
 import hr.foi.air.popapp.ui.theme.POPAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,9 +22,111 @@ class MainActivity : ComponentActivity() {
             POPAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-
+                    RegistrationPage()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun RegistrationPage() {
+    var firstName by remember {
+        mutableStateOf("")
+    }
+    var lastName by remember {
+        mutableStateOf("")
+    }
+    var username by remember {
+        mutableStateOf("")
+    }
+    var email by remember {
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
+    var confirmPassword by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+        Text(
+            text = "Create an Account",
+            style = MaterialTheme.typography.h4
+        )
+
+
+        OutlinedTextField(
+            label = { Text("First name") },
+            value = firstName,
+            onValueChange = { firstName = it },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+        )
+
+
+        OutlinedTextField(
+            label = { Text("Last name") },
+            value = lastName,
+            onValueChange = { lastName = it },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+        )
+
+
+        OutlinedTextField(
+            label = { Text("Username") },
+            value = username,
+            onValueChange = { username = it },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+        )
+
+
+        OutlinedTextField(
+            label = { Text("Email") },
+            value = email,
+            onValueChange = { email = it },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+        )
+
+
+        OutlinedTextField(
+            label = { Text("Password") },
+            value = password,
+            onValueChange = { password = it },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+
+        OutlinedTextField(
+            label = { Text("Confirm password") },
+            value = confirmPassword,
+            onValueChange = { confirmPassword = it },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+        Button(onClick = {}) {
+            Text("Register")
         }
     }
 }
