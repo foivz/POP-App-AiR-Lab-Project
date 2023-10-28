4d5858cef9a41f6dcdaa3b5f57ee3666e8146415
Mislav Matijević <mislav.mim@gmail.com>
2023-10-28 12:54:31 +0200

    Demonstrate mutable state in Jetpack Compose. Using "by remember mutableStateOf" syntax, the Greeting composable function became a stateful component. Adding a Column element just made the text indicating the current counter value and the button appear one after another. Changing the modifier on the Column positioned the two elements in the middle of the screen.

--- app/src/main/java/hr/foi/air/popapp/MainActivity.kt
+++ app/src/main/java/hr/foi/air/popapp/MainActivity.kt
@@ -3,11 +3,15 @@ package hr.foi.air.popapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import hr.foi.air.popapp.ui.theme.POPAppTheme
@@ -29,6 +33,18 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        var counter by remember { mutableStateOf(0) }
+        Text(text = "You've pressed a button $counter times")
+        Button(onClick = { counter++ }) {
+            Text(text = "Press me")
+        }
+    }
 }
 
 @Preview(showBackground = true)
