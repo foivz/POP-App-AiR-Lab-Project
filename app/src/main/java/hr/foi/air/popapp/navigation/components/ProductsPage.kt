11d076ad57e329996ae5c8545cee0bcb0625eb65
Mislav Matijević <mislav.mim@gmail.com>
2023-11-01 23:58:34 +0100

    Implement displaying of products. ProductsPage now displays a list of products received from the ProductsService. Receiving is done via ProductsRequestHandler.

--- app/src/main/java/hr/foi/air/popapp/navigation/components/ProductsPage.kt
+++ app/src/main/java/hr/foi/air/popapp/navigation/components/ProductsPage.kt
@@ -1,9 +1,52 @@
 package hr.foi.air.popapp.navigation.components.registration
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import hr.foi.air.popapp.context.Auth
+import hr.foi.air.popapp.core.login.network.ResponseListener
+import hr.foi.air.popapp.core.login.network.models.ErrorResponseBody
+import hr.foi.air.popapp.core.login.network.models.SuccessfulResponseBody
+import hr.foi.air.popapp.ws.models.responses.Product
+import hr.foi.air.popapp.ws.request_handlers.ProductsRequestHandler
 
 @Composable
 fun ProductsPage() {
-    Text(text = "Welcome to the products page!")
+    val (products, setProducts) = remember { mutableStateOf<MutableList<Product>>(mutableListOf()) }
+
+    val productsRequestHandler = ProductsRequestHandler(Auth.loggedInUserData!!.jwt)
+
+    productsRequestHandler.sendRequest(object : ResponseListener {
+        override fun <T> onSuccessfulResponse(response: SuccessfulResponseBody<T>) {
+            println(response)
+            val productsArray = response.data[0]
+            setProducts((productsArray as Array<Product>).toMutableList())
+        }
+
+        override fun onErrorResponse(response: ErrorResponseBody) {
+            println(response)
+        }
+
+        override fun onNetworkFailure(t: Throwable) {
+            println("Error contacting network...")
+        }
+    })
+
+    if (products.isEmpty()) {
+        CircularProgressIndicator(
+            modifier = Modifier.fillMaxSize()
+        )
+    } else {
+        LazyColumn {
+            items(products) {
+                Text(it.name ?: "?")
+            }
+        }
+    }
 }
