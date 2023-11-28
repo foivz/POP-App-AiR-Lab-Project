b5ac5300a280e6276a85a9ba83071b10a90d0e2e
Mislav Matijević <mislav.mim@gmail.com>
2023-11-28 20:08:57 +0100

    Fix 'WS' to finally use safe protocol. Pure HTTP no longer works. Thankfully.

--- ws/src/main/java/hr/foi/air/popapp/ws/network/NetworkService.kt
+++ ws/src/main/java/hr/foi/air/popapp/ws/network/NetworkService.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object NetworkService {
-    private const val BASE_URL = "http://ase.foi.hr/pop/api/v2/"
+    private const val BASE_URL = "https://ase.foi.hr/pop/api/v2/"
 
     private var instance: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
