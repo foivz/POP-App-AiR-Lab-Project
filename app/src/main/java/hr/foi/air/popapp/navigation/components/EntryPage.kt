af7d0b311be2ca667ca6d943a0829099a8a6a75e
Mislav Matijević <mislav.mim@gmail.com>
2023-12-02 00:06:50 +0100

    Reformat EntryPage code.

--- app/src/main/java/hr/foi/air/popapp/navigation/components/EntryPage.kt
+++ app/src/main/java/hr/foi/air/popapp/navigation/components/EntryPage.kt
@@ -1,8 +1,21 @@
 package hr.foi.air.popapp.navigation.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -13,6 +26,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import hr.foi.air.popapp.R
+import hr.foi.air.popapp.ui.components.AdMobBanner
 import hr.foi.air.popapp.ui.theme.Shapes
 
 @Composable
@@ -74,9 +88,17 @@ fun EntryPage(
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.Center
             ) {
-                Divider(modifier = Modifier.fillMaxWidth(0.47f), color = Color.LightGray, thickness = 2.dp)
+                Divider(
+                    modifier = Modifier.fillMaxWidth(0.47f),
+                    color = Color.LightGray,
+                    thickness = 2.dp
+                )
                 Text("OR")
-                Divider(modifier = Modifier.fillMaxWidth(), color = Color.LightGray, thickness = 2.dp)
+                Divider(
+                    modifier = Modifier.fillMaxWidth(),
+                    color = Color.LightGray,
+                    thickness = 2.dp
+                )
             }
 
             Button(
