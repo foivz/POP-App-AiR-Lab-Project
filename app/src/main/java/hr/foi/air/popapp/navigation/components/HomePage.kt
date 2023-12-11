8b0a13d9a1e4f55012bf8ccba607184039a26ced
Mislav Matijević <mmatijevi@foi.hr>
2023-12-11 09:12:27 +0100

    Formatting changes. Android Studio decided to reformat the entire HomePage.

--- app/src/main/java/hr/foi/air/popapp/navigation/components/HomePage.kt
+++ app/src/main/java/hr/foi/air/popapp/navigation/components/HomePage.kt
@@ -1,13 +1,17 @@
 package hr.foi.air.popapp.navigation.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.List
@@ -23,7 +27,7 @@ import hr.foi.air.popapp.context.Auth
 import hr.foi.air.popapp.ui.components.MenuItem
 import hr.foi.air.popapp.ui.theme.POPAppTheme
 import hr.foi.air.popapp.ui.theme.Typography
-import java.util.*
+import java.util.Locale
 
 val menuItemsAll = listOf(
     "Balance" to Icons.Default.Star,
@@ -38,29 +42,51 @@ val menuItemsSeller = listOf(
 
 @Composable
 fun HomePage(onMenuOptionSelected: (optionName: String) -> Unit) {
+    Scaffold(
+        topBar = {
+            Row(
+                modifier = Modifier.background(MaterialTheme.colors.primaryVariant),
+                horizontalArrangement = Arrangement.End
+            ) {
+                Button(onClick = {
 
-    Column(modifier = Modifier.background(MaterialTheme.colors.background)) {
-        Text(
-            text = "Welcome, ${Auth.loggedInUserData?.firstName ?: "[Unknown name]"}",
-            style = Typography.h4,
-            modifier = Modifier.fillMaxWidth(),
-            color = MaterialTheme.colors.primaryVariant,
-            textAlign = TextAlign.Center
-        )
-
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(20.dp)
-        ) {
-            items(getMenuItemsListBasedOnRole()) { (text, icon) ->
-                MenuItem(text, icon) {
-                    onMenuOptionSelected(text.lowercase(Locale.getDefault()))
+                }) {
+                    Text(text = "Purchase a different app theme")
                 }
             }
+        }
+    ) {
 
-            items(menuItemsAll) { (text, icon) ->
-                MenuItem(text, icon) { onMenuOptionSelected(text.lowercase(Locale.getDefault())) }
+        Column(
+            modifier = Modifier
+                .background(MaterialTheme.colors.background)
+                .padding(it)
+        ) {
+            Text(
+                text = "Welcome, ${Auth.loggedInUserData?.firstName ?: "[Unknown name]"}",
+                style = Typography.h4,
+                modifier = Modifier.fillMaxWidth(),
+                color = MaterialTheme.colors.primaryVariant,
+                textAlign = TextAlign.Center
+            )
+
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(20.dp)
+            ) {
+                items(getMenuItemsListBasedOnRole()) { (text, icon) ->
+                    MenuItem(text, icon) {
+                        onMenuOptionSelected(text.lowercase(Locale.getDefault()))
+                    }
+                }
+
+                items(menuItemsAll) { (text, icon) ->
+                    MenuItem(
+                        text,
+                        icon
+                    ) { onMenuOptionSelected(text.lowercase(Locale.getDefault())) }
+                }
             }
         }
     }
